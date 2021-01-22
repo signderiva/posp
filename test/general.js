e22b7359467fe32a7f2829237c766314d5f9eab2
Michael Vergoz <m.vergoz@vergoz.ch>
2021-01-22 15:25:23 +0100

    Challenge working

--- test/general.js
+++ test/general.js
@@ -3,56 +3,52 @@
  */
 const posp = require('../index')
 
-var globalHdl
+
 var globalStored
 
 const global = {}
 
 describe('General', function () {
-  it('creating posp instance', function (done) {
-    globalHdl = new posp({})
-    done()
-  })
 
   it('Generate POSP password', function (done) {
     this.timeout(4000);
-    globalHdl.generate("Super password", (res) => {
-      // console.log("generate", res)
+    posp.generate("Super password", (err, res) => {
+      // if(!res.conf.hash) return(done("No hash specified"))
+      console.log("generate", res)
       globalStored = res.string;
       done()
     })
   })
 
-  // it('Verify with good password', function (done) {
-  //   this.timeout(4000);
-  //   globalHdl.verify(globalStored, "Super password", (res) => {
-  //     if(res !== true) done("Passwords are egal")
-  //     else done()
-  //   })
-  // })
+  it('Verify with good password', function (done) {
+    this.timeout(4000);
+    posp.verify(globalStored, "Super password", (authed, res) => {
+      if (authed !== true) done("Passwords are egal")
+      else done()
+    })
+  })
 
-  // it('Verify with wrong password', function (done) {
-  //   this.timeout(4000);
-  //   globalHdl.verify(globalStored, "Not good", (res) => {
-  //     if(res === true) done("Passwords must be different")
-  //     else done()
-  //   })
-  // })
-
+  it('Verify with wrong password', function (done) {
+    this.timeout(4000);
+    posp.verify(globalStored, "Not good", (authed, res) => {
+      if (authed === true) done("Passwords must be different")
+      else done()
+    })
+  })
 
   it('Generate server-side authentification challenge', function (done) {
-    globalHdl.generateChallenge(globalStored, {}, (res) => {
+    posp.generateChallenge(globalStored, {}, (err, res) => {
       if(!res.hasOwnProperty("packet")) return(done("Invalid packet"))
       if(!res.hasOwnProperty("string")) return(done("Invalid string"))
       global.serverChallenge = res.string;
-      // console.log(res)
+      console.log("generate challenge", res)
       done()
     })
   })
 
   it('Client response to a server authentification challenge', function (done) {
     this.timeout(4000);
-    globalHdl.responseChallenge(global.serverChallenge, "Super password", (res) => {
+    posp.responseChallenge(global.serverChallenge, "Super password", (err, res) => {
       global.clientChallengeResponse = res.string;
       // console.log('challenge response', global.serverChallenge, res)
       done()
@@ -60,9 +56,8 @@ describe('General', function () {
   })
 
   it('Server verify client response challenge', function (done) {
-    globalHdl.verifyChallenge(global.clientChallengeResponse, globalStored, (err) => {
+    posp.verifyChallenge(global.clientChallengeResponse, globalStored, (err) => {
       if(err) return(done(err))
-      // console.log('verify response', err)
       done()
     })
   })
